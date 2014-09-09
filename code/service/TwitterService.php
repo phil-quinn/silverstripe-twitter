086f8c4d71dbbba1f8379d75affa67ba4c20a307
Phil Quinn <phil@quinn.com>
2014-09-09 14:45:56 -0700

    added support for media, urls, videos

--- code/service/TwitterService.php
+++ code/service/TwitterService.php
@@ -151,6 +151,9 @@ class TwitterService implements ITwitterService {
 			'User' => $tweet->user->screen_name,
 			'AvatarUrl' => $tweet->user->profile_image_url,
 			'Content' => $this->parseText($tweet),
+			'Media'	=> $this->parseMedia($tweet),
+			'URLs'	=> $this->parseURLs($tweet),
+			'Videos'	=> $this->parseVideos($tweet),
 			'Link' => "{$profileLink}/status/{$tweetID}",
 			'ProfileLink' => $profileLink,
 			'ReplyLink' => "https://twitter.com/intent/tweet?in_reply_to={$tweetID}",
@@ -158,6 +161,275 @@ class TwitterService implements ITwitterService {
 			'FavouriteLink' => "https://twitter.com/intent/favorite?tweet_id={$tweetID}"
 		);
 	}
+
+	/**
+	 * Look for urls in the entities of a tweet and convert it 
+	 * to usable ArrayData in an ArrayList
+	 * 
+	 * @param stdObject $tweet Tweet object
+	 * @return ArrayList 
+	 */
+	public function parseURLs($tweet) {
+		$urls = new ArrayList();
+		if (isset($tweet->entities->urls)) {
+			foreach($tweet->entities->urls as $item) {
+				$urls->push( new ArrayData(array(
+					'URL' => $item->url,
+					'ExpandedURL' => $item->expanded_url,
+					'DisplayURL' => $item->display_url,
+				)));
+			}
+			return $urls;
+		}
+	}
+
+	/**
+	 * Look for videos (YouTube or Vimeo) in the urls in the 
+	 * entities of a tweet and convert it 
+	 * to usable ArrayData in an ArrayList
+	 * 
+	 * @param stdObject $tweet Tweet object
+	 * @return ArrayList 
+	 */
+	public function parseVideos($tweet) {
+		$videos = new ArrayList();
+		if (isset($tweet->entities->urls)) {
+			foreach($tweet->entities->urls as $item) {
+				$url = $item->expanded_url;
+
+				if (self::is_youtube($url)) {
+					$type = 'youtube';
+					$id = self::youtube_video_id($url);
+					$thumbs = new ArrayData( array(
+						'Small' => self::youtube_thumb_url($id, $size = 's'),
+						'Medium' => self::youtube_thumb_url($id, $size = 'm'),
+						'Large' => self::youtube_thumb_url($id, $size = 'l'),
+					));
+				}
+				else if (self::is_vimeo($url)) {
+					$type = 'vimeo';
+					$id = self::vimeo_video_id($url);
+					$thumbs = new ArrayData( array(
+						'Small' => self::vimeo_thumb_url($id, $size = 's'),
+						'Medium' => self::vimeo_thumb_url($id, $size = 'm'),
+						'Large' => self::vimeo_thumb_url($id, $size = 'l'),
+					));
+				}
+				else {
+					// if not YouTube or Vimeo, skip
+					continue;
+				}
+				$videos->push( new ArrayData(array(
+					'URL' => $item->url,
+					'ExpandedURL' => $item->expanded_url,
+					'DisplayURL' => $item->display_url,
+					'Type' => $type,
+					'Thumbs' => $thumbs,
+					'IFrameURL' => self::video_iframe_url($url),
+					'IFrameCode' => self::video_iframe_code($url),
+				)));
+			}
+			return $videos;
+		}
+	}
+
+
+	/**
+	 * Look for media in the entities of a tweet and convert it 
+	 * to usable ArrayData in an ArrayList
+	 * 
+	 * @param stdObject $tweet Tweet object
+	 * @return ArrayList 
+	 */
+	public function parseMedia($tweet) {
+		$media = new ArrayList();
+		if (isset($tweet->entities->media)) {
+			foreach($tweet->entities->media as $item) {
+				$media->push( new ArrayData(array(
+					'ID' => $item->id,
+					'MediaURLNoProtocol' => preg_replace('/^https?:/i','',$item->media_url),
+					'MediaURL' => $item->media_url,
+					'MediaURLHTTPS' => $item->media_url_https,
+					'URL' => $item->url,
+					'DisplayURL' => $item->display_url,
+					'ExpandedURL' => $item->expanded_url,
+					'Type' => $item->type,
+					'Sizes' => $this->parseSizes($item->sizes),
+				)));
+			}
+			return $media;
+		}
+	}
+
+	/**
+	 * Convert entity media sizes to an ArrayList
+	 * 
+	 * @param stdObject $sizes the sizes object
+	 * @return ArrayData
+	 */
+	public function parseSizes($sizes) {
+		if ($sizes) {
+			$size_labels = array(
+				'small' 	=> 'Small',
+				'medium' 	=> 'Medium',
+				'large' 	=> 'Large',
+				'thumb' 	=> 'Thumb',
+			);
+			$size_array = array();
+			foreach ($size_labels as $key => $label) {
+				$size_array[$label] = new ArrayData(array(
+					'Width'=>$sizes->$key->w,
+					'Height'=>$sizes->$key->h,
+					'Resize'=>$sizes->$key->resize,
+				));
+			}
+			return new ArrayData( $size_array );
+		}
+		return false;
+	}
+
+
+
+
+	/**
+	 * Is this YouTube?
+	 *
+	 * @param $url Video URL
+	 * @return boolean
+	*/
+	public static function is_youtube($url) {
+		return (preg_match('/youtu\.be/i', $url) || preg_match('/youtube\.com\/watch/i', $url));
+	}
+
+	/**
+	 * Is this Vimeo?
+	 *
+	 * @param $url Video URL
+	 * @return boolean
+	*/
+	public static function is_vimeo($url = false) {
+		return (preg_match('/vimeo\.com/i', $url));
+	}
+
+	/**
+	 * Parse the ID from a YouTube URL
+	 *
+	 * @param $url Video URL
+	 * @return string
+	*/
+	public static function youtube_video_id($url) {
+		if(self::is_youtube($url)) {
+			$pattern = '/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/';
+			preg_match($pattern, $url, $matches);
+			if (count($matches) && strlen($matches[7]) == 11) {
+				return $matches[7];
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Parse the ID from a Vimeo URL
+	 *
+	 * @param $url Video URL
+	 * @return string
+	*/
+	public static function  vimeo_video_id($url) {
+		if(self::is_vimeo($url)) {
+			$pattern = '/\/\/(www\.)?vimeo.com\/(\d+)($|\/)/';
+			preg_match($pattern, $url, $matches);
+			if (count($matches)) {
+				return $matches[2];
+			}
+		}
+		return false;
+	}
+
+
+	/**
+	 * Returns the iframe code for this video
+	 *
+	 * @param $url Video URL
+	 * @param $width iframe width (defaults to '640')
+	 * @param $height iframe height (defaults to '360')
+	 * @param $fixed Do not resize to fit with JS (defaults to false)
+	 * @return string
+	*/
+	public static function video_iframe_code($url, $width = '640', $height = '360', $fixed = false) {
+		$class = $fixed ? 'video-iframe fixed' : 'video-iframe resize';
+		$video_iframe_url = self::video_iframe_url($url);
+		if (self::is_vimeo($url)) {
+			$code = self::vimeo_video_id($url);
+			return "<iframe class=\"$class\" src=\"$video_iframe_url\" width=\"$width\" height=\"$height\" frameborder=\"0\" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>";
+		}
+		else if (self::is_youtube($url)) {
+			$code = self::youtube_video_id($url);
+			return "<iframe class=\"$class\" width=\"$width\" height=\"$height\" src=\"$video_iframe_url\" frameborder=\"0\" allowfullscreen></iframe>";
+		}
+	}
+	/**
+	 * Returns the iframe url for this video
+	 *
+	 * @param $url Video URL
+	 * @return string
+	*/
+	public static function video_iframe_url($url) {
+		if ($url) {
+			if (self::is_vimeo($url)) {
+				$code = self::vimeo_video_id($url);
+				return "//player.vimeo.com/video/$code";
+			}
+			else if (self::is_youtube($url)) {
+				$code = self::youtube_video_id($url);
+				return "//www.youtube.com/embed/$code";
+			}
+		}
+	}
+
+	/**
+	 * Gets a Vimeo thumbnail url
+	 *
+	 * @param mixed $id A vimeo id (ie. 1185346)
+	 * @param mixed $size 's' small, 'm' medium, 'l' large
+	 * @return thumbnail's url
+	*/
+	static function vimeo_thumb_url($id, $size = 'm') {
+		$data = @file_get_contents("http://vimeo.com/api/v2/video/$id.json");
+		if ($data) {
+			$data = json_decode($data);
+			switch($size) {
+				case 's':
+					return $data[0]->thumbnail_small;
+				case 'm':
+					return $data[0]->thumbnail_medium;
+				case 'l':
+					return $data[0]->thumbnail_large;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Gets a YouTube thumbnail url
+	 *
+	 * @param mixed $id A YouTube id (ie. mXWSkBp0z8Y)
+	 * @param mixed $size 's' small, 'm' medium, 'l' large
+	 * @return string
+	*/
+	public static function youtube_thumb_url($id, $size = 'm') {
+		switch($size) {
+			case 's':
+				return "http://img.youtube.com/vi/$id/default.jpg";
+			case 'm':
+				return "http://img.youtube.com/vi/$id/0.jpg";
+			case 'l':
+				return "http://img.youtube.com/vi/$id/maxresdefault.jpg";
+		}
+		return false;
+	}
+
+
+
 
 	/**
 	 * Inject a hyperlink into the body of a tweet
